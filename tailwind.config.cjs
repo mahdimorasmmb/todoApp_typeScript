afadf343eaf4c7f8d9cb312a3b644a23aeb73542
mahdi morasaeiyan <mahdimoras@gmail.com>
2023-02-08 13:31:02 +0330

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'header-color':"#2f80ed"
+      }
+    },
+  },
+  plugins: [],
+}
