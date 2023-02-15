abe02a8da4958e2f1cd73e52bcf56457283c4653
mahdi morasaeiyan <mahdimoras@gmail.com>
2023-02-15 13:15:15 +0330

    use History API for manger ruting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,60 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { httpBatchLink } from "@trpc/client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { trpc } from "./utils/trpc";
 
 import TodoContainer from "./containers/TodoContainer";
 import Header from "./partials/Header/Header";
 import EditContainer from "./containers/EditContainer";
 import { withSideDrawer } from "./hoc/withSideDrawer";
+import StatsContainer from "./containers/StatsContainer";
 
-const EditContainerWithSideDrawer = withSideDrawer(EditContainer)
+const EditContainerWithSideDrawer = withSideDrawer(EditContainer);
 export function App() {
+  const [location, setLocation] = useState(window.location.pathname);
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
           url: "http://localhost:4000/trpc",
-          // optional
-          // headers() {
-          //   return {
-          //     authorization: getAuthCookie(),
-          //   };
-          // },
         }),
       ],
     })
   );
+
+  useEffect(() => {
+    document.querySelectorAll("a").forEach((el) => {
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const link = (e.target as HTMLAnchorElement).href;
+
+        if (window.history.state?.pathLink !== link) {
+          window.history.pushState({ pathLink: link }, "", link);
+          setLocation(window.location.pathname);
+        }
+      });
+    });
+
+    window.addEventListener("popstate", (e) => {
+      setLocation((e.target as Window).location.pathname);
+    });
+  }, []);
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <Header />
         <div className="mr-auto ml-auto w-[500px]">
-          <TodoContainer />
-          <EditContainerWithSideDrawer  />
+          {location === "/" && (
+            <>
+              <TodoContainer />
+              <EditContainerWithSideDrawer />
+            </>
+          )}
+
+          {location === "/stats" && <StatsContainer />}
         </div>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
--- frontend/src/partials/Header/Header.tsx
+++ frontend/src/partials/Header/Header.tsx
@@ -6,9 +6,9 @@ const Header = () => {
     <header className="bg-PRIMARY_COLOR mb-5 flex px-3 py-5 text-white">
       <h1 className="text-lg font-bold">Todo App</h1>
       <nav className="m-auto">
-        <HeaderLink href="./">Home</HeaderLink>
-        <HeaderLink href="./">Statas</HeaderLink>
-        <HeaderLink href="./">About</HeaderLink>
+        <HeaderLink href="/">Home</HeaderLink>
+        <HeaderLink href="/about">About</HeaderLink>
+        <HeaderLink href="/stats">Statas</HeaderLink>
       </nav>
     </header>
   );
