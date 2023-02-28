c0b0162b54cc9f2df08ab62437c6bcb33ea3be63
mahdi morasaeiyan <mahdimoras@gmail.com>
2023-03-01 02:35:09 +0330

    fuck

--- frontend/src/utils/trpc.ts
+++ frontend/src/utils/trpc.ts
@@ -3,10 +3,12 @@ import { createTRPCReact } from '@trpc/react-query';
 import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import { AppRouter } from '../../../server/src';
 
-export const trpc = createTRPCReact<AppRouter>();
 
 
 type RouterInput = inferRouterInputs<AppRouter>
 type RouterOutput = inferRouterOutputs<AppRouter>;
 
 export type TodoType = RouterOutput['todo']
+
+export const trpc = createTRPCReact<AppRouter>();
+
